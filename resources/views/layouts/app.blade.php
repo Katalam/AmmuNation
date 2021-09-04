2cb177f4311342bc8d2edfd185efd4de045d82a6
Bruno <39590058+Katalam@users.noreply.github.com>
2021-09-04 17:20:57 +0200

    Changed favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,15 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#90292c">
+    <meta name="msapplication-TileColor" content="#90292c">
+    <meta name="theme-color" content="#90292c">
 </head>
 
 <body class="bg-gray-50 antialiased font-sans">
