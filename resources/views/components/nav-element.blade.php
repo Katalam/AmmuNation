dfbc21c8ebbc2c5f6a87fcbabfbecaccdcb4a14a
Bruno <39590058+Katalam@users.noreply.github.com>
2021-09-04 17:01:55 +0200

    Fixed missing white border after livewire update

--- resources/views/components/nav-element.blade.php
+++ resources/views/components/nav-element.blade.php
@@ -1,8 +1,8 @@
-@props(['first' => false, 'route', 'title'])
+@props(['first' => false, 'route', 'title', 'croute'])
 
 <li {{ $attributes }}>
     <a href="{{ route($route) }}">
-        <div class="flex items-center text-sm text-gray-50 pb-1 {{ Route::currentRouteName() == $route ? 'border-white' : 'border-transparent' }} border-b hover:border-white {{ $first ? '' : 'xl:ml-6 2xl:ml-11' }}">
+        <div class="flex items-center text-sm text-gray-50 pb-1 {{ $croute == $route ? 'border-white' : 'border-transparent' }} border-b hover:border-white {{ $first ? '' : 'xl:ml-6 2xl:ml-11' }}">
             {{ $slot }}
             <p class="ml-1.5">{{ $title }}</p>
         </div>
